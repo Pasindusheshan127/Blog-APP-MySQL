a3beb34a4e1dd6112cd9f5678a4819dec6ae1ca6
Pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-07-19 21:35:40 +0530

    footer design

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,13 @@
+import logo from "../img/logo.jpg";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <img src={logo} alt="" />
+      <span>
+        Made with ❤ and <b>ReactJS</b>
+      </span>
+    </div>
+  );
 };
 
 export default Footer;

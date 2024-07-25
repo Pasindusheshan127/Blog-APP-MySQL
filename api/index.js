12102c3e90db110e057813cb4760a560550a03ac
Pasindusheshan127 <pasinduhettiarachchi201@gmail.com>
2024-07-25 08:09:01 +0530

    use JWT to login

--- api/index.js
+++ api/index.js
@@ -3,10 +3,12 @@ import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auth.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 // Configure CORS
 app.use(
